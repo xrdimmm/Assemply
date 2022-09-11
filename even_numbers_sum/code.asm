8cb8bd9fe731cb473e37cfc55edbb750ff436204
Xristos_Dim <xristosdim99@gmail.com>
2022-09-11 12:45:29 +0300

    Update code.asm

--- even_numbers_sum/code.asm
+++ even_numbers_sum/code.asm
@@ -18,8 +18,8 @@ loop:
 	addi $a0, $a0, 4               # Pointer move 4 bytes forward (i++).
 	slt  $t1, $zero, $t0           # possitive number check  | if  $zero < $t0 then $t1 = 1 else $t1 = 0 
 	beq  $t1, $zero, loop          # if $t1 == 0 which means the number is negative, go to label loop else do nothing ( proceed to next line).
-	andi $t1, $t0, 1               # less significant bit check to determine number nature || If bit  == 0 then then $t1 == 0 (number is odd). If bit is 1 then $t1 = 1 (number is even)
-	bne  $t1, $zero, loop          # if value in $t1 == 0 (number is odd) goto label loop else do nothing( proceed to next line)
+	andi $t1, $t0, 1               # less significant bit check to determine number nature || If bit  == 0 then then $t1 == 0 (number is even). If bit is 1 then $t1 = 1 (number is odd)
+	bne  $t1, $zero, loop          # if value in $t1 != 0 (number IS_NOT even) goto label loop else do nothing( proceed to next line)
 	add  $v0, $v0, $t0             # Number is even . $v0 = $v0 + $t0  ($v0 -> sum , $t0 -> number)
 	j loop			       # goto label loop.
 exit:
